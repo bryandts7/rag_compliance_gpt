12f4108242eb3c427f66b21e2d9c0f829f55e92c
bryandts7 <bryandts7@gmail.com>
2024-07-17 15:05:08 +0700

    [feat] push docstore to mongodb

--- constants/docs.py
+++ constants/docs.py
@@ -1,15 +1,31 @@
+import ast
 from  langchain.schema import Document
-import json
-from typing import Iterable
 
-def load_docs_from_jsonl(file_path)->Iterable[Document]:
-    array = []
-    with open(file_path, 'r') as jsonl_file:
-        for line in jsonl_file:
-            data = json.loads(line)
-            obj = Document(**data)
-            array.append(obj)
-    return array
+from database.database import rekam_jejak_docstore, ketentuan_terkait_docstore
 
-REKAM_DOCS = load_docs_from_jsonl(r"constants/rekam_jejak_docs_langchain.jsonl")
-KETENTUAN_DOCS = load_docs_from_jsonl(r"constants/ketentuan_terkait_docs_langchain.jsonl")
+def str_to_dict(s):
+    # Replace the ObjectId string with a placeholder
+    s = s.replace("ObjectId(", "").replace(")", "")
+
+    # Use ast.literal_eval to safely evaluate the string
+    d = ast.literal_eval(s)
+    
+    # Manually convert the _id field back to an ObjectId
+    if '_id' in d:
+        del d['_id']
+    
+    return d
+
+def db_to_docs(docs):
+    res = []
+    for doc in docs:
+        dic = str_to_dict(doc.page_content)
+        lang_doc = Document(page_content=dic['page_content'], metadata=dic['metadata'])
+        res.append(lang_doc)    
+    return res
+
+REKAM_DOCSTORE =  rekam_jejak_docstore().load()
+KETENTUAN_DOCSTORE = ketentuan_terkait_docstore().load()
+
+REKAM_DOCS = db_to_docs(REKAM_DOCSTORE)
+KETENTUAN_DOCS = db_to_docs(KETENTUAN_DOCSTORE)
--- database/database.py
+++ database/database.py
@@ -1,5 +1,6 @@
 from pinecone import Pinecone, ServerlessSpec
 from langchain_pinecone import PineconeVectorStore
+from langchain_community.document_loaders.mongodb import MongodbLoader
 
 from utils.azure_openai import azure_embeddings
 
@@ -10,8 +11,22 @@ dotenv.load_dotenv()
 
 embeddings = azure_embeddings()
 
+# Pinecone Database
 pinecone_api_key = os.environ.get("PINECONE_API_KEY")
 pc = Pinecone(api_key=pinecone_api_key)
+
+# MongoDB Database
+mongo_uri = os.environ.get("MONGO_URI")
+REKAM_JEJAK_DOCSTORE = MongodbLoader(
+    connection_string= mongo_uri, # type: ignore
+    db_name="docstore",
+    collection_name="rekam_jejak",
+)
+KETENTUAN_TERKAIT_DOCSTORE = MongodbLoader(
+    connection_string= mongo_uri, # type: ignore
+    db_name="docstore",
+    collection_name="ketentuan_terkait",
+)
 
 def rekam_jejak_vector():
     index_name = "rekam-jejak"
@@ -19,9 +34,15 @@ def rekam_jejak_vector():
     rekam_jejak_vector = PineconeVectorStore(index_name=index_name, embedding=embeddings)
     return rekam_jejak_vector
 
+def rekam_jejak_docstore():
+    return REKAM_JEJAK_DOCSTORE
+
 def ketentuan_terkait_vector():
     index_name = "ketentuan-terkait"
     index = pc.Index(index_name)
     ketentuan_terkait_vector = PineconeVectorStore(index_name=index_name, embedding=embeddings)
 
     return ketentuan_terkait_vector
+
+def ketentuan_terkait_docstore():
+    return KETENTUAN_TERKAIT_DOCSTORE
